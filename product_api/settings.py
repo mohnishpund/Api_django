2a59b3b9b651276dbe8425a31c869fb78d13d18e
mohnish pund <mohnishpund11@gmail.com>
2022-07-06 10:20:13 -0700

    massage 6

--- product_api/settings.py
+++ product_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-c*qem1s%c_h^=t--1d(edbu8y(w7x-rd*f0l043hydani_^#$c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
